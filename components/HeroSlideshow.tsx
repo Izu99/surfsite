1f89570052777928341004cccef04c26abd54722
izu99 <isuruumanga123@gmail.com>
2026-04-11 21:23:05 +0530

    surf site new

--- components/HeroSlideshow.tsx
+++ components/HeroSlideshow.tsx
@@ -7,6 +7,10 @@ import { cn } from '@/lib/utils'
 
 const slides = [
   {
+    src: 'https://plus.unsplash.com/premium_photo-1673783807734-3dd0ffbf6784?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'Surfing action at Hikkaduwa Beach',
+  },
+  {
     src: 'https://plus.unsplash.com/premium_photo-1672510003630-18d2535419ef?q=80&w=2070&auto=format&fit=crop',
     alt: 'Surf action at Hikkaduwa Beach',
   },
